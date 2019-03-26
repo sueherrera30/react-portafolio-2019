2e75d4a439dcf1c89493814495389fa91ee5e6cb
sue <sue@MacBook-Pro-de-sue.local>
2019-03-26 16:06:06 -0600

    set up and first component

---
+++ src/components/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const About = () => { 
+const name = "sue"
+return (
+    <>
+    <h1>Hola</h1> 
+    <h5>{name} !</h5>
+    </>
+    )
+}
+   
+export default About;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import About from './components/About';
+
+ReactDOM.render(<About /> , document.getElementById('App'));
+
+
+
+
+
