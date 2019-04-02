002502532d8b1b3739c4e0e8b85d2c674b5f3234
sue <sue@MacBook-Pro-de-sue.local>
2019-04-02 14:53:14 -0600

    trying toggle with hook useState

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './containers/App';
 import ButtonTest from './components/Button';
+import ButtonHooks from './components/ButtonHooks';
 
-ReactDOM.render(<ButtonTest /> , document.getElementById('App'));
+ReactDOM.render(<ButtonHooks /> , document.getElementById('App'));
 
 
 
