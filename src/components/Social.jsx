8908089b964c84fc07535b63bd965cedf425cd5e
sue <sue@MacBook-Pro-de-sue.local>
2019-04-11 09:56:24 -0500

    getting out styles drom main social component

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -1,63 +1,12 @@
 import React from 'react';
 import styled, { ThemeProvider } from 'styled-components';
+import {SocialMain, SocialList, SingleSocial,github,facebook,mail, linkedin,SocialIcon,} from '../styled/social/icons';
 
-const github = {
-    color: "#fcb576",
-    transform: "rotate(30deg)",
-}
-
-const linkedin = {
-    color: "#446d6f",
-    transform: "rotate(-30deg)",
-}
-
-const facebook = {
-    color: "#99bd95",
-    transform: "rotate(-20deg)",
-}
-
-const mail = {
-    color: "papayawhip",
-    transform: "rotate(35deg)",
-}
-const SocialMain = styled.div`
-    width: 100%;
-    background-color: rgba(54, 95, 100, 0.3);
-    height: 100px;
-    margin: 0;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    li {
-        list-style-type: none;
-    }
-`;
-
-const SocialList = styled.ul`
-    width: 50%; 
-    display: flex;
-    justify-content: space-around;
-    align-items: center;
-    li {
-        list-style-type: none;
-    }
-`;
-
-
-const SingleSocial = styled.a`
-    text-decoration: none;
-    font-size: 2rem;
-`;
-
-const SocialIcon = styled.i`
-    color: ${props => props.theme.color};
-    transform: ${props => props.theme.transform}
-`;
 const getStyle = (name) => {
-     if(name == 'github' ) return github;
-     if(name == 'linkedin' ) return linkedin;
-     if(name == 'facebook' ) return facebook;
-     if(name == 'mail' ) return mail;
+    if(name == 'github' ) return github;
+    if(name == 'linkedin' ) return linkedin;
+    if(name == 'facebook' ) return facebook;
+    if(name == 'mail' ) return mail;
 }
 
 const Social = () => ( 
@@ -95,8 +44,7 @@ const Social = () => (
             </SingleSocial>
             </li> 
         </SocialList>
-    </SocialMain>
-       
+    </SocialMain>       
 );
 export default Social;
 
--- src/styled/social/icons.jsx
+++ src/styled/social/icons.jsx
@@ -1,10 +1,69 @@
-// import React from 'react';
-// import styled from 'styled-components';
+import React from 'react';
+import styled from 'styled-components';
 
-// const iconStyle = styled.i`
-//     color: ${ props => props.theme.color};
-// `;
+const SocialMain = styled.div`
+    width: 100%;
+    background-color: rgba(54, 95, 100, 0.3);
+    height: 100px;
+    margin: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    li {
+        list-style-type: none;
+    }
+`;
 
-// const SocialIcon = () => <iconStyle className={}></iconStyle>;
+const SocialList = styled.ul`
+    width: 50%; 
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    li {
+        list-style-type: none;
+    }
+`;
+const SingleSocial = styled.a`
+    text-decoration: none;
+    font-size: 2rem;
+`;
+const github = {
+    color: "#fcb576",
+    transform: "rotate(30deg)",
+    fontSize: "5rem",
+}
 
-// export default SocialIcon;
+const linkedin = {
+    color: "#446d6f",
+    transform: "rotate(-30deg)",
+    fontSize: "3rem",
+}
+
+const facebook = {
+    color: "#99bd95",
+    transform: "rotate(-20deg)",
+    fontSize: "2rem",
+}
+
+const mail = {
+    color: "papayawhip",
+    transform: "rotate(35deg)",
+    fontSize: "2rem",
+}
+
+const SocialIcon = styled.i`
+    color: ${props => props.theme.color};
+    transform: ${props => props.theme.transform};
+    font-size: ${props => props.theme.fontSize};
+`;
+
+export {
+    SocialMain,
+    SocialList,
+    SingleSocial,
+    github,
+    facebook,
+    mail,
+    linkedin,
+    SocialIcon,
+}
