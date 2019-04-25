efe2be3422412c85c648d9f7b5a163c591a782c0
sue <sue@MacBook-Pro-de-sue.local>
2019-04-25 14:17:51 -0500

    first part of json

--- src/hooks/useGetData.js
+++ src/hooks/useGetData.js
@@ -3,17 +3,16 @@ const information = '../src/information.json'
 
 // 1. Creating general function 
 const useGetData = () => {
-    // 2. using hook that will manage the state(right now it's empty)
+// 2. using hook that will manage the state(right now it's empty)
     const [myData, setData] = useState([]);
-    // 3.using second hook to do the fetch, it works as the life stycle. 
+// 3.using second hook to do the fetch, it works as the life stycle. 
     useEffect(() => {
         fetch(information)
-        //  4. call up information and changing to a json object.
+//  4. call up information and changing to a json object.
         .then(res => res.json())
-        // 5. adding received data with the function.
+// 5. adding received data with the function.
         .then(data => setData(data))
     },[])
     return myData;
 }
-
 export default useGetData;
